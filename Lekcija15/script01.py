8d60bf0b884203189ed8f087a7f0dbf358c10e33
Islam Spahic <islamspahic8@gmail.com>
2020-02-14 21:18:37 +0100

    Lekcija15 | primjer01

---
+++ Lekcija15/script01.py
@@ -0,0 +1,9 @@
+koordinate = {}
+
+koordinate[ (10, 20, 30) ] = 15.4
+koordinate[ (10, -5, 11) ] = 20.0
+
+print(koordinate)
+
+if (10, 25, 11) in koordinate.keys():
+    print ( koordinate[(10, 25, 11)])
